a8eb5f70dc018582b32a1a9e01822e9f902d6416
root <root@vmadminwer.5hcdfv4ate1uvftdu4th12tbqf.cx.internal.cloudapp.net>
2020-05-07 11:28:46 +0000

    Prod code

---
+++ terrafrom-code/Tomcat-deploy/local_exec.tf
@@ -0,0 +1,51 @@
+#terraform {
+ # backend "local" {
+  #  path = "/tmp/terraform/workspace/terraform.tfstate"
+  #}
+
+#}
+
+provider "aws" {
+  region = "us-east-1"
+#  shared_credentials_file = "/root/.aws/credentials"
+  access_key = "AKIAXQHX55V5CWHF4CBY"
+  secret_key = "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"
+}
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-039a49e70ea773ffc"
+  instance_type          = "t2.micro"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
+
+}
+
+resource "null_resource" "remote-exec-1" {
+    connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    host        = "${aws_instance.backend.public_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install python sshpass -y",
+    ]
+  }
+}
+
+resource "null_resource" "ansible-main" {
+provisioner "local-exec" {
+  command = <<EOT
+        sleep 100;
+        > jenkins-ci.ini;
+        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+        export ANSIBLE_HOST_KEY_CHECKING=False;
+        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+        ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini ./ansible-code/05-Tomcat/playbook.yaml -u ubuntu -v
+    EOT
+}
+}
+
---
+++ terrafrom-code/Tomcat-deploy/variables.tf
@@ -0,0 +1,15 @@
+variable "key_name" {
+  default = "Mykey"
+}
+
+variable "pvt_key" {
+  default = "/root/.ssh/balu-aws.pem"
+}
+
+variable "us-east-zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "sg-id" {
+  default = "sg-063b9875d598e4401"
+}
